cdda46c800188a7b2237eb889df428a9fd6ba19c
Vitor Lima <viitorlima2000@gmail.com>
2025-03-09 03:35:41 -0300

    Update hackerrank_nested_lists.py

--- hackerrank_nested_lists.py
+++ hackerrank_nested_lists.py
@@ -12,4 +12,31 @@ for i in range(N):
     main_list.append(deepcopy(aux_list))
     aux_list.clear()
 
-print(main_list)
+min_score = 999
+for student in main_list:
+    if student[1] < min_score:
+        min_score = student[1]
+
+while_counter = 0
+forif_counter = 1
+while while_counter < forif_counter:
+    while_counter += 1
+    for student in main_list:
+        if student[1] == min_score:
+            forif_counter += 1
+            main_list.remove(student)
+    
+
+min_score = 999
+for student in main_list:
+    if student[1] < min_score:
+        min_score = student[1]
+
+students_list = list()
+for student in main_list:
+    if student[1] == min_score:
+        students_list.append(student[0])
+
+students_list.sort()
+for student in students_list:
+    print(student)
